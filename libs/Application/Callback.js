a7b5a664001fb78fccc5a9d373abdd502a2bc289
Alin Roman <alinroman@Alins-MacBook-Pro.local>
2016-03-02 16:00:30 +0200

    Fixed popup.

--- libs/Application/Callback.js
+++ libs/Application/Callback.js
@@ -1,30 +1,52 @@
 var consts = require('../consts.js');
 
+/**
+ * @constructor
+ */
 function Callback() {
     this.button = consts.Buttons.Middle;
     this.event = consts.ButtonEvents.Press;
     this.action = null;
 }
 
+/**
+ * Sets the button to listen to
+ * @param button
+ * @returns {Callback}
+ */
 Callback.prototype.setButton = function(button) {
     this.button = button;
     return this;
 };
 
+/**
+ * Sets the event to listen to
+ * @param event
+ * @returns {Callback}
+ */
 Callback.prototype.setEvent = function(event) {
     this.event = event;
     return this;
 };
 
+/**
+ * Sets the action to perform on event
+ * @param action
+ * @returns {Callback}
+ */
 Callback.prototype.setAction = function(action) {
     this.action = action;
     return this;
 };
 
+/**
+ * Returns a serialized object of the callback
+ * @returns {Object}
+ */
 Callback.prototype.toObject = function() {
     var callback = {
-        button: this.content,
-        event: this.label
+        button: this.button,
+        event: this.event
     };
 
     if (this.action) {
