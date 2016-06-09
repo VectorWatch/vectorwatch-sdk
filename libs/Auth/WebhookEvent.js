4d9c0f3d397123c94f292ac0ac7938b88355f0e9
Robert Miroiu <robert.miroiu@vectorwatch.com>
2016-06-09 12:58:06 +0300

    merge

--- libs/Auth/WebhookEvent.js
+++ libs/Auth/WebhookEvent.js
@@ -12,6 +12,20 @@ function WebhookEvent() {
 }
 util.inherits(WebhookEvent, Event);
 
+WebhookEvent.prototype.getQuery = function() {
+    if(this.req.body && this.req.body.query) {
+        return this.req.body.query;
+    }
+    return undefined;
+};
+
+WebhookEvent.prototype.getHeaders = function() {
+    if(this.req.body && this.req.body.headers) {
+        return this.req.body.headers;
+    }
+    return undefined;
+};
+
 WebhookEvent.prototype.getResponseClass = function() {
     return require('./WebhookResponse.js');
 };
