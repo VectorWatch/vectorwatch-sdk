37e280d7f95d01c2d8c044293cd1e326a2070ae0
Radu <radu.vlad@vectorwatch.com>
2016-11-24 17:20:29 +0200

    Added records in webhook emit. Other fixes.

--- libs/Auth/WebhookEvent.js
+++ libs/Auth/WebhookEvent.js
@@ -8,8 +8,30 @@ var Event = require('../Event.js');
 function WebhookEvent() {
     Event.apply(this, arguments);
 
+    var _this = this;
     this.eventName = 'webhook';
+    this.emit = function(response) {
+
+    	_this.getServer().getStorageProvider().getAllUserSettingsAsync().then(function (records) {
+    		if(records && records.length) {
+    			records.forEach(function (record) {
+    				record.pushUpdate = function(streamText, delay) {
+    					return _this.getServer().pushStreamValue(record.channelLabel, streamText, delay);
+    				}
+    				record.pushNotification = function(notificationText, delay) {
+    					return _this.getServer().pushNotification(record.channelLabel, notificationText, delay);
+    				}
+    			});
+    		}
+    		else {
+    			records = [];
+    		}
+
+    		return _this.getServer().emit(_this.getEventName(), _this, response, records);
+    	});
+    };    
 }
+
 util.inherits(WebhookEvent, Event);
 
 WebhookEvent.prototype.getResponseClass = function() {
